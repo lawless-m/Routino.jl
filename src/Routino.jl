c756fb4d4af260651823b344e4ab872140803747
lawless-m <matt-github@steponnopets.net>
2022-01-19 14:15:46 +0000

    better types

--- src/Routino.jl
+++ src/Routino.jl
@@ -5,12 +5,14 @@ export route, distance
 const BINARY = "/usr/bin/routino-router"
 const DATADIR = "/trip/osm"
 
-function route(lat1::Real, lon1::Real, lat2::Real, lon2::Real; binary=BINARY, datadir=DATADIR, prefix="GB")
-    split(String(read(`$binary --quickest --output-text --output-stdout --dir=$datadir --prefix=$prefix --lat1=$lat1 --lon1=$lon1 --lat2=$lat2 --lon2=$lon2`)), "\n")
+const LoLa = typeof((lo=0.0, la=0.0))
+
+function route(wayp1::LoLa, wayp2::LoLa; binary=BINARY, datadir=DATADIR, prefix="GB")
+    split(String(read(`$binary --quickest --output-text --output-stdout --dir=$datadir --prefix=$prefix --lon1=$(wayp1.lo) --lat1=$(wayp1.la) --lon2=$(wayp2.lo) --lat2=$(wayp2.la)`)), "\n")
 end
 
-function distance(lat1::Real, lon1::Real, lat2::Real, lon2::Real; binary=BINARY, datadir=DATADIR, prefix="GB")
-    tline = filter(t->occursin("Waypt#2", t), route(lat1, lon1, lat2, lon2;  binary, datadir, prefix))
+function distance(wayp1::LoLa, wayp2::LoLa; binary=BINARY, datadir=DATADIR, prefix="GB")
+    tline = filter(t->occursin("Waypt#2", t), route(wayp1, wayp2;  binary, datadir, prefix))
     if length(tline) == 1
         parts = split(tline[1], "\t")
         km = 1.2parse(Float64, split(strip(parts[5]), " ")[1])
@@ -19,9 +21,9 @@ function distance(lat1::Real, lon1::Real, lat2::Real, lon2::Real; binary=BINARY,
     end
 end
 #==
-lon1, lat1 = ( -1.7587022270945216,  53.67050050773988) # HD2
-lon2, lat2 = ( -1.6091688317085435, 53.74083763716689) # pcode_centre(idx, "LS27 0AL")
-route(lat1, lon1, lat2, lon2)
+wayp1 = (lo=-1.7587022270945216, la=53.67050050773988) # HD2
+wayp2 = (lo=-1.6091688317085435, la=53.74083763716689) # pcode_centre(idx, "LS27 0AL")
+route(wayp1, wayp2)
 ==#
 
 ###
