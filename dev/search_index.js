1d8206c9ced3abc15e5da615dd137e89e2d39463
Documenter.jl <documenter@juliadocs.github.io>
2022-01-19 09:58:15 +0000

    build based on d1fd0f4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Routino.jl","page":"Routino.jl","title":"Routino.jl","text":"","category":"section"},{"location":"","page":"Routino.jl","title":"Routino.jl","text":"Documentation for Routino.jl","category":"page"},{"location":"","page":"Routino.jl","title":"Routino.jl","text":"","category":"page"}]
+}
