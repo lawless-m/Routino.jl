f9117a500166ca37e3bbfaa53105a113d838548c
lawless-m <matt-github@steponnopets.net>
2022-01-21 12:58:37 +0000

    now works with .so

--- src/Routino.jl
+++ src/Routino.jl
@@ -1,14 +1,12 @@
 module Routino
 
+using CCallHelp
+
 export route, distance, Connection, find_waypoint, add_waypoint!, calculate_route
 
-const BINARY = "/home/matt/routino-3.3.3/src/router" 
-#"/usr/bin/routino-router"
-
+const BINARY = "/usr/bin/routino-router"
 const DATADIR = "/trip/osm"
-
-const LIB = "/home/matt/routino-3.3.3/src/libroutino.so" 
-#"/usr/lib/libroutino.so.0"
+const LIB = "/usr/lib/libroutino.so.0"
 const ROUTINO_API_VER = Cint(8)
 const PROFILES = "/usr/share/routino/profiles.xml"
 const TRANSLATIONS = "/usr/share/routino/translations.xml"
@@ -18,7 +16,7 @@ const LoLa = typeof((lo=0.0, la=0.0))
 #==
  routino-router  --quickest --output-text --output-stdout --dir=/trip/osm --prefix=GB --lon1=-1.7587022270945216 --lat1=53.67050050773988 --lon2=-1.6091688317085435 --lat2=53.74083763716689 --profiles=/usr/share/routino/profiles.xml --translations=/usr/share/routino/translations.xml 
  ==#
-function run(wayp1::LoLa, wayp2::LoLa; binary=BINARY, datadir=DATADIR, prefix="GB", profiles=PROFILES, translations=TRANSLATIONS)
+function shell(wayp1::LoLa, wayp2::LoLa, binary=BINARY; datadir=DATADIR, prefix="GB", profiles=PROFILES, translations=TRANSLATIONS)
     try 
         read(`$binary --quiet --quickest --output-text --output-stdout --dir=$datadir --prefix=$prefix --lon1=$(wayp1.lo) --lat1=$(wayp1.la) --lon2=$(wayp2.lo) --lat2=$(wayp2.la) --profiles=$profiles --translations=$translations`)
     catch
@@ -26,16 +24,16 @@ function run(wayp1::LoLa, wayp2::LoLa; binary=BINARY, datadir=DATADIR, prefix="G
     end
 end
 
-function route(wayp1::LoLa, wayp2::LoLa; binary=BINARY, datadir=DATADIR, prefix="GB")
-    split(String(run(wayp1, wayp2;  binary, datadir, prefix)), "\n")
+function route(wayp1::LoLa, wayp2::LoLa, binary; datadir=DATADIR, prefix="GB")
+    split(String(shell(wayp1, wayp2, binary; datadir, prefix)), "\n")
 end
 
-function distance(wayp1::LoLa, wayp2::LoLa; binary=BINARY, datadir=DATADIR, prefix="GB", default=nothing)
-    tline = filter(t->occursin("Waypt#2", t), route(wayp1, wayp2;  binary, datadir, prefix))
+function distance(wayp1::LoLa, wayp2::LoLa, binary, datadir=DATADIR, prefix="GB", default=nothing)
+    tline = filter(t->occursin("Waypt#2", t), route(wayp1, wayp2, binary; datadir, prefix))
     if length(tline) == 1
         parts = split(tline[1], "\t")
-        km = 1.2parse(Float64, split(strip(parts[5]), " ")[1])
-        mins = 1.2parse(Float64, split(strip(parts[6]), " ")[1])
+        km = parse(Float64, split(strip(parts[5]), " ")[1])
+        mins = parse(Float64, split(strip(parts[6]), " ")[1])
         (;km, mins)
     else
         default
@@ -65,38 +63,17 @@ struct Connection
     end
 end 
 
-function uload_cstring(ptr)
-    string = Vector{UInt8}()
-    i = 1
-    while (c = unsafe_load(ptr, i)) != 0
-        push!(string, c)
-        i += 1
-    end
-    String(string)
-end
-function uload_vstrings(list)
-    strings = Vector{String}()
-    i = 1
-    while (stringp = unsafe_load(list,i)) != C_NULL
-        push!(strings, uload_cstring(stringp))
-        i += 1
-    end
-    strings
-end
-
-
 const DB = Ptr{Cvoid}
 const Profile = Ptr{Cvoid}
 const Waypoint = Ptr{Cvoid}
 const Translation = Ptr{Cvoid}
 const ProgressFunc= Ptr{Cvoid}
 
-const ListString = Ptr{Ptr{UInt8}}
+const PUchars = Ptr{Cuchar}
+const ListString = Ptr{PUchars}
 
-const OutputPtr = Ptr{Cvoid}
-
-struct Output
-    next::Ptr{Output}
+struct COutput
+    next::Ptr{COutput}
     lon::Cfloat
     lat::Cfloat
     dist::Cfloat
@@ -105,17 +82,17 @@ struct Output
     type::Cint
     turn::Cint
     bearing::Cint
-    name::Cstring
-    desc1::Cstring
-    desc2::Cstring
-    desc3::Cstring
+    name::PUchars
+    desc1::PUchars
+    desc2::PUchars
+    desc3::PUchars
 end
 
 check_api_version(ver=ROUTINO_API_VER) = (@ccall LIB.Routino_Check_API_Version(ver::Cint)::Cint) == 0
 open_database(path=DATADIR, prefix="GB") = @ccall LIB.Routino_LoadDatabase(path::Cstring, prefix::Cstring)::DB
 load_xml_profiles(filename=PROFILES) = (@ccall LIB.Routino_ParseXMLProfiles(filename::Cstring)::Cint) == 0
 load_xml_translations(filename=TRANSLATIONS) = (@ccall LIB.Routino_ParseXMLTranslations(filename::Cstring)::Cint) == 0
-get_profile_names() = @ccall LIB.Routino_GetProfileNames()::ListString
+get_profile_names() = vec_string_from_ptr_ptr_uchar(@ccall LIB.Routino_GetProfileNames()::ListString)
 get_profile(name::AbstractString) = @ccall LIB.Routino_GetProfile(name::Cstring)::Profile
 validate_profile(db, profile) = (@ccall LIB.Routino_ValidateProfile(db::DB, profile::Profile)::Cint) == 0
 get_translation(name="en") = @ccall LIB.Routino_GetTranslation(name::Cstring)::Translation
@@ -124,7 +101,7 @@ find_waypoint(lo, la, connection=Connection()) = find_waypoint(lo, la, connectio
 calculate_route(waypoints::Vector{Waypoint}, options=Cint[1,9,512]; connection=Connection()) = calculate_route(waypoints, connection.db, connection.profile, connection.translation, options)
 
 function calculate_route(waypoints::Vector{Ptr{Cvoid}}, db, profile, translation, options=Cint[1,9,512])
-    @ccall LIB.Routino_CalculateRoute(db::DB, profile::Profile, translation::Translation, waypoints::Waypoint, length(waypoints)::Cint, reduce((a,i) -> a |= i, options, init=Cint(0))::Cint, C_NULL::Ptr{Cvoid})::Ptr{Output}
+    @ccall LIB.Routino_CalculateRoute(db::DB, profile::Profile, translation::Translation, waypoints::Waypoint, length(waypoints)::Cint, reduce((a,i) -> a |= i, options, init=Cint(0))::Cint, C_NULL::Ptr{Cvoid})::Ptr{COutput}
 end
 
 function calculate_route(connection, options=Cint[1,9,512])
@@ -141,18 +118,19 @@ function add_waypoint!(lo::Float64, la::Float64, connection::Connection)
     end
     false
 end
-#==
-needed adding manually to the C code
-print_profile(profile) = ccall((:Routino_PrintProfile, LIB), Cvoid, (Profile,), profile)
-==#
 
-function find()
-    # using Routino
-    connection = Connection("motorcar")
-    add_waypoint!(-1.7587022270945216, 53.67050050773988, connection)
-    add_waypoint!(-1.6091688317085435, 53.74083763716689, connection)
-    calculate_route(connection)
+function distance(ptr::Ptr{COutput})
+    if ptr == C_NULL
+        return
+    end
+    out = unsafe_load(ptr, 1)
+    if out.next == C_NULL
+        return (km=round(Int, out.dist), mins=round(Int, out.time))
+    end
+    distance(out.next)
 end
+
+
 
 ###
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,16 @@ using Test
     if isfile(Routino.BINARY)
         wayp1 = (lo=-1.7587022270945216, la=53.67050050773988) # HD2
         wayp2 = (lo=-1.6091688317085435, la=53.74083763716689) # pcode_centre(idx, "LS27 0AL")
-        @test distance(wayp1, wayp2) == (km = 23.16, mins = 15.6)
+        @test distance(wayp1, wayp2, Routino.BINARY) == (km = 19.3, mins = 13)
     end
+    if isfile(Routino.LIB)
+        connection = Connection("foot")
+        add_waypoint!(-1.7587022270945216, 53.67050050773988, connection)
+        add_waypoint!(-1.6091688317085435, 53.74083763716689, connection)
+        out = calculate_route(connection)
+        @test out != C_NULL
+        @test typeof(out) == Ptr{Routino.COutput}
+        @test distance(out) == (km = 15, mins = 229)
+    end
+
 end
